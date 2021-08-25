7cd99f8790fe15894c3e591ac629f45781ef0a6d
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2021-08-25 09:47:24 +0100

    fix home about

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,6 +10,7 @@ import bloom from "../../images/bloom.svg";
 import insider from "../../images/insider.svg";
 import times from "../../images/times.svg";
 import cnbc from "../../images/cnbc.svg";
+import workgroup from "../../images/workgroup.jpg";
 
 const categories = [
   {
@@ -26,11 +27,11 @@ const categories = [
   },
   {
     name: "REAL ESTATE",
-    img: "https://images.unsplash.com/photo-1610998342124-c4fcba4cf4bf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    img: "https://images.unsplash.com/photo-1599393292508-483954a8183e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=899&q=80",
   },
   {
     name: "SOLAR ENERGY",
-    img: "https://images.unsplash.com/photo-1584276433295-4b49a252e5ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=742&q=80",
+    img: "https://images.unsplash.com/photo-1574170623305-76d87a78cfcc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
   },
   {
     name: "INFORMATION TECHNOLOGY",
@@ -145,6 +146,134 @@ function Home() {
           <img alt="img" src={bloom} />
         </div>
       </div>
+      <div className="home__second-about">
+        <div className="home__second-about__img-box">
+          <img
+            src={workgroup}
+            alt="about_img"
+            className="home__second-about__img-box__img"
+          />
+        </div>
+        <div className="home__second-about__text-box">
+          <h2 className="home__second-about__text-box__heading">ABOUT US</h2>
+          <div className="home__second-about__text-box__main">
+            <h3 className="home__second-about__text-box__main__heading">
+              Passionate Marketing Experts
+            </h3>
+            <p>
+              We have successful businesses create their brand presence and
+              achieve their goals. Our process is the plan to certify your
+              organization's company and outfit your business with the marketing
+              tools needed to succeed. Talk to us today about how we can support
+              your growth, limit your turnover, and put you on a solid track to
+              success and profit.
+            </p>
+          </div>
+          <div className="home__second-about__text-box__main">
+            <h3>Our Analytical Approach</h3>
+            <p>
+              Our service includes a comprehensive consult to help identify gaps
+              and opportunities, and results in a comprehensive report that
+              includes a project plan with timelines a cost analysis. Our
+              individualized plans are made up of quality services that will
+              help you get there quickly and smoothly.
+            </p>
+          </div>
+          <div>
+            <h3>Industry Wide Success</h3>
+            <p>
+              We work with a large number of clients from various industries,
+              including:
+            </p>
+            <ul>
+              <li>Oil & Gas</li>
+              <li>Agriculture</li>
+              <li>Automotive</li>
+              <li>Construction</li>
+              <li>Information Technology</li>
+              <li>Real Estate</li>
+              <li>Travel & Tourism</li>
+            </ul>
+            <p>
+              Our marketing experts are ready assist you in developing a
+              marketing plan that drives your business.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="home__second-contact">
+        <h3 className="home__second-contact__heading">CONTACT US</h3>
+        <div className="home__second-contact__content">
+          <div className="home__second-contact__content__form">
+            <h4>Drop Us A line</h4>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Email" />
+              <textarea placeholder="Message"></textarea>
+              <button type="submit">SEND</button>
+            </form>
+          </div>
+          <div className="home__second-contact__content__details">
+            <div>
+              <h4>Better yet, see us in person!</h4>
+              <p>
+                We love to work with you, so feel free to contact us anytime.
+              </p>
+            </div>
+            <div>
+              <h4>WALANCO GROUP</h4>
+              <p>
+                WALANCO GROUP PO BOX 322 SHEIK MOHAMMED BIN RASHID BLIVD-
+                DOWNTOWN, DUBAI, UNITED ARAB EMIRATES
+              </p>
+              <p>
+                contact us @walancogroup@gmail.com & walancogroup@yahoomail.com
+              </p>
+            </div>
+            <div>
+              <h3>Open 24hours</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="address-wrapper">
+        <h3>ADDRESS</h3>
+        <div className="address">
+          <div>
+            <h4>UNITED ARAB EMIRATE</h4>
+            <ul>
+              <li>WALANCO GROUP</li>
+              <li>PO BOX 322</li>
+              <li>SHEIK MOHAMMED BIN RASHID BLIVD- DOWNTOWN </li>
+              <li>DUBAI, UNITED ARAB EMIRATES</li>
+              <li>MOBILE NUMBER: +971528137250</li>
+              <li>EMAIL: walancogroup@gmail.com </li>
+            </ul>
+          </div>
+          <div>
+            <h4>UNITED STATE</h4>
+            <ul>
+              <li>WALANCO GROUP</li>
+              <li>PO BOX 322</li>
+              <li>725 5TH AVENUE NEW YORK CITY </li>
+              <li>NEW YORK, UNITED STATES</li>
+              <li>MOBILE NUMBER: +1814808-5605 </li>
+              <li>EMAIL: walancogroup@gmail.com </li>
+            </ul>
+          </div>
+          <div>
+            <h4>UNITED ARAB EMIRATE</h4>
+            <ul>
+              <li>WALANCO GROUP</li>
+              <li>PO BOX E14 5HQ</li>
+              <li>CANADA SQUARE,</li>
+              <li>LONDON, UNITED KINGDOM</li>
+              <li>MOBILE NUMBER:+44714612-6855 </li>
+              <li>EMAIL: walancogroup@gmail.com </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className="newsletter">
         <div className="newsletter-desc">
           <h2>Weekly Luxury Newsletter</h2>
@@ -154,6 +283,7 @@ function Home() {
             inside stories and tips from our experts.
           </p>
         </div>
+
         <div className="newsletter-form">
           <form onSubmit={(e) => e.preventDefault()}>
             <div>
